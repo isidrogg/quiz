f78d9498d01cd63597c1e0fc3ced022af6eca3a4
Isidro García <isidro.gg@gmail.com>
2015-08-03 12:16:45 +0200

    Despliegue DB en Heroku

---
+++ models/models.js
@@ -0,0 +1,41 @@
+var path = require('path');
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
+//Cargar Modulo ORM
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize(DB_name, user, pwd, 
+  { dialect:  protocol,
+    protocol: protocol,
+    port:     port,
+    host:     host,
+    storage:  storage,  // solo SQLite (.env)
+    omitNull: true      // solo Postgres
+  }      
+);
+
+//Importar la definición de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+
+exports.Quiz = Quiz; //Exportar definición de la tabal Quiz
+
+//sequelize.sync() crea e inicializa tabla de preguntas en DB
+sequelize.sync().success(function() {
+	Quiz.count().success(function (count){
+		if(count===0) {
+			Quiz.create({ pregunta: 'Capital de Italia',
+						  respuesta: 'Roma'
+						})
+			.success(function(){console.log('Base de datos inicializa')});
+		};
+	});
+});
